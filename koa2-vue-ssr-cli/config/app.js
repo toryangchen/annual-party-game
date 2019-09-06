91a5aebe3fd0cf0270c4352fb4fdc320faac230f
Toryang <toryangchen@outlook.com>
2019-09-06 17:07:12 +0800

    项目移动

--- koa2-vue-ssr-cli/config/app.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  app: {
-    port: 3000,
-    devHost: '127.0.0.1',
-    open: true,
-  },
-};
--- koa2-vue-ssr-cli/config/lib/index.js
+++
@@ -1,5 +0,0 @@
-// https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-shared-utils/lib
-
-['env', 'openBrowser'].forEach(m => {
-  Object.assign(exports, require(`./${m}`));
-});
